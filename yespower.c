6e662419927fd9472f855560c709421741a28dfa
y-chan <34832037+y-chan@users.noreply.github.com>
2019-09-12 22:15:45 +0900

    fix error

--- yespower.c
+++ yespower.c
@@ -4,10 +4,12 @@
  */
 int yespower_hash(const char *input, char *output)
 {
-	.version = YESPOWER_1_0,
-    	.N = 2048,
-    	.r = 32,
-    	.pers = "Satoshi Nakamoto 31/Oct/2008 Proof-of-work is essentially one-CPU-one-vote",
-    	.perslen = 74
+	yespower_params_t params = {
+		.version = YESPOWER_1_0,
+		.N = 2048,
+		.r = 32,
+		.pers = "Satoshi Nakamoto 31/Oct/2008 Proof-of-work is essentially one-CPU-one-vote",
+		.perslen = 74
+	};
 	return yespower_tls(input, 80, &params, (yespower_binary_t *) output);
 }

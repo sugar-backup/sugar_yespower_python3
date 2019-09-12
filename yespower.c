9f7b9b59af2cd421da126e814ce1906d78a2f4a6
cryptozeny <37016180+cryptozeny@users.noreply.github.com>
2019-09-12 22:04:50 +0900

    clearfication yespower paramters

--- yespower.c
+++ yespower.c
@@ -4,6 +4,10 @@
  */
 int yespower_hash(const char *input, char *output)
 {
-	yespower_params_t params = {YESPOWER_1_0, 2048, 32, "Satoshi Nakamoto 31/Oct/2008 Proof-of-work is essentially one-CPU-one-vote", 74};
+	.version = YESPOWER_1_0,
+    	.N = 2048,
+    	.r = 32,
+    	.pers = "Satoshi Nakamoto 31/Oct/2008 Proof-of-work is essentially one-CPU-one-vote",
+    	.perslen = 74
 	return yespower_tls(input, 80, &params, (yespower_binary_t *) output);
 }
